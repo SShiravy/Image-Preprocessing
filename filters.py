46b10118da85e17d9bb50e7e3e2365043e8031fd
Soheil <soheilprogrammer0@gmail.com>
2023-05-28 19:30:52 +0330

    filters.py updated and results added

--- filters.py
+++ filters.py
@@ -6,6 +6,10 @@ import numpy as np
 
 class ImageFilter:
     def __init__(self, img_dir='images'):
+        self.laplacian_imgs = []
+        self.sobel_imgs = []
+        self.gray_imgs = []
+        self.threshold = []
         self.morphology_imgs = []
         self.blended = []
         self.binary_imgs = []
@@ -17,21 +21,39 @@ class ImageFilter:
         for img in os.listdir(self.img_dir):
             if img[-3:] == 'png':
                 self.images.append(cv2.imread(self.img_dir + '/' + img))
+        return self.images
 
-    def hls_filter(self,img_list=None):
+    def hls_filter(self, img_list=None):
+        """
+        good for flowers and fruits detection
+        :param img_list: list of input images, if leave this, then it is self.images
+        :return: HLS filtered images
+        """
         img_list = self.images if img_list is None else img_list
         for img in img_list:
             self.hls_imgs.append(cv2.cvtColor(img, cv2.COLOR_RGB2HLS))
         return self.hls_imgs
 
-    def binary_filter(self,img_list=None):
+    def binary_filter(self, img_list=None):
+        """
+        good for flowers and fruits detection
+        :return: HLS filtered images
+        :param img_list: list of input images, if leave this, then it is self.images
+        :return: binary filtered images
+        """
         img_list = self.images if img_list is None else img_list
         for img in img_list:
             _, binary_imgs = cv2.threshold(img, img.max() / 2, img.max(), cv2.THRESH_BINARY)
+            binary_imgs = cv2.cvtColor(binary_imgs, cv2.COLOR_BGR2RGB)
             self.binary_imgs.append(binary_imgs)
         return self.binary_imgs
 
-    def blended_filter(self,img_list=None):
+    def blended_filter(self, img_list=None):
+        """
+        good for node detection
+        :param img_list: list of input images, if leave this, then it is self.images
+        :return: blended filtered images
+        """
         img_list = self.images if img_list is None else img_list
         for img in img_list:
             sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
@@ -39,19 +61,47 @@ class ImageFilter:
             self.blended.append(cv2.addWeighted(src1=sobelx, alpha=0.5, src2=sobely, beta=0.5, gamma=0))
         return self.blended
 
-    def morphology_filter(self,img_list=None):
+    def morphology_filter(self, img_list=None):
+        """
+        good for node detection
+        :param img_list: list of input images, if leave this, then it is self.blended
+        :return: morphology filtered images
+        """
         img_list = self.blended if img_list is None else img_list
         for blended_img in img_list:
-            self.morphology_imgs.append(cv2.morphologyEx(blended_img, cv2.MORPH_GRADIENT, np.ones((2,2), np.uint8)))
+            self.morphology_imgs.append(cv2.morphologyEx(blended_img, cv2.MORPH_GRADIENT, np.ones((2, 2), np.uint8)))
         return self.morphology_imgs
 
-    def save_imgs(self,list_images,dir,pre_name='test_filters'):
+    def gray_filter(self, img_list=None):
+        """
+        good for extend dataset
+        :param img_list: list of input images, if leave this, then it is self.images
+        :return: gray filtered images
+        """
+        img_list = self.images if img_list is None else img_list
+        for img in img_list:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            self.gray_imgs.append(gray)
+        return self.gray_imgs
+
+    def sobel_filter(self, img_list=None):
+        """
+        good for node detection
+        :param img_list: list of input images, if leave this, then it is self.gray_imgs
+        :return: sobel filtered images
+        """
+        img_list = self.gray_imgs if img_list is None else img_list
+        for img in img_list:
+            sobel = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+            self.sobel_imgs.append(sobel)
+        return self.sobel_imgs
+
+    def save_imgs(self, list_images, dir, pre_name='test_filters'):
         i = 1
         try:
             for img in list_images:
                 cv2.imwrite(f'{dir}/{pre_name}_{i}.png', img)
-                i+=1
+                i += 1
             return True
         except:
             return False
-
--- main.py
+++ main.py
@@ -2,17 +2,21 @@
 import matplotlib.pyplot as plt
 from filters import ImageFilter
 import cv2
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     img_filter = ImageFilter()
-    img_filter.read_imgs()
+    images = img_filter.read_imgs()
     hls = img_filter.hls_filter()[0]
-    binary = img_filter.binary_filter()[0]
     blended = img_filter.blended_filter()[0]
     morphology = img_filter.morphology_filter()[0]
-    img_filter.save_imgs([hls,binary,blended,morphology],'resultImages')
-    # plt.imshow(img_filter.hls_imgs[0])
-    # plt.show()
+    binary = img_filter.binary_filter(images)[0]
+    gray = img_filter.gray_filter()[0]
+    sobel = img_filter.sobel_filter()[0]
+
+    img_filter.save_imgs([hls,blended,morphology,binary,gray,sobel], 'resultImages')
+    plt.imshow(sobel)
+    plt.show()
